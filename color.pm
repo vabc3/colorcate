50d277fb6186f6fba09ef0badfaf6c7b257a1025
Karata <vabc3h@gmail.com>
2017-04-07 22:48:03 +0800

    Add input output.

--- color.pm
+++ color.pm
@@ -3,9 +3,25 @@ use warnings;
 
 package Color;
 
-use overload "-" = \&mysub;
+use overload "-" => \&mysub;
+my $C = 3; #rgb
+my $EXP = 2;
 
+sub new {
+    my $class = shift;
+    my $val = shift;
+    return bless $val => $class
+}
 
 sub mysub{
-    say "s;";
+    my ($x, $y) = @_;
+    my $gap = 0;
+
+    for (my $i=0; $i<$C; $i++){
+        $gap += abs($$x[$i]-$$y[$i])**$EXP;
+    }
+
+    return $gap;
 }
+
+1
